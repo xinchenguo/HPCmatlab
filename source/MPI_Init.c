f0e244ceb7ff3328a0f1af7d3582fb33303385f4
Mukul Dave <mdave@login001.oscar.ccv.brown.edu>
2016-11-23 22:49:48 -0500

    Added LGPL copyright notice to top of each source file.

--- source/MPI_Init.c
+++ source/MPI_Init.c
@@ -1,4 +1,29 @@
 /*
+    HPCmatlab - Framework for Message Passing in Matlab
+    Copyright (C) 2013-2016  Xinchen Guo, Mukul Dave, Ayush Mishra & Mohamed Sayeed
+
+    This file is part of HPCmatlab.
+
+    HPCmatlab is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    HPCmatlab is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+
+    Email IDs: xinchen.guo@asu.edu, mukul92@gmail.com,
+               ayush02mishra@gmail.com, msayeed@asu.edu
+*/
+
+/*
  *  Currently does not support arguments
  */
 
